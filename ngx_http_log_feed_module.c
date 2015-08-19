6682d5e71809571ac16f7dca80dce519b6b331ba
zhigang.zhang@guoguogo360.com <zzgang2008@gmail.com>
2015-08-19 13:08:36 -0400

    Change param name

--- ngx_http_log_feed_module.c
+++ ngx_http_log_feed_module.c
@@ -196,7 +196,7 @@ static feed_log_line_var_t feed_log_line_vars[] = {
     FEED_LOG_LINE_VAR_ENTRY(status),
     FEED_LOG_LINE_VAR_ENTRY(cookies),
     FEED_LOG_LINE_VAR_ENTRY(user_agent),
-    FEED_LOG_LINE_VAR_ARG_ENTRY(host, "VA.url"),
+    FEED_LOG_LINE_VAR_ARG_ENTRY(host, "VA.host"),
     FEED_LOG_LINE_VAR_ARG_ENTRY(request, "VA.request"),
     FEED_LOG_LINE_VAR_ARG_ENTRY(referer, "VA.ref"), 
     FEED_LOG_LINE_VAR_ENTRY(args), //must be last
